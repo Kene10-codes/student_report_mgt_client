24dcd1b27015467ee755c195d8bf8a5bef98dd8f
Kene10-codes <nwobodokenechukwu2@gmail.com>
2025-01-31 10:27:31 -0800

    developinf reusable components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
-import  { SignupComponent } from './signup/signup.component'
+import  { SignupComponent } from './signup/signup.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+    {path: '', component: AppComponent },
     { path: 'login', component: LoginComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'signup', component: SignupComponent}
