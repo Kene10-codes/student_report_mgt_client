adb0c5ce542bc68f03b466e0aa1d5ff9caa603b5
Kene10-codes <nwobodokenechukwu2@gmail.com>
2025-01-31 19:07:58 -0800

    Working on the login UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup
 
+
+  constructor(private formBuilder: FormBuilder){
+   this.loginForm = this.formBuilder.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  })
+  }
 }
